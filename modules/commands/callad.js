671b272f800adab3155368729c1c08ec3481eac9
EliansSotero <149171618+EliansSotero@users.noreply.github.com>
2024-01-20 20:15:12 +0800

    Update callad.js

--- modules/commands/callad.js
+++ modules/commands/callad.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "callad",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "NTKhang, ManhG Fix Get",
+	credits: "𝙀𝙇.𝙄𝘼𝙉𝙎 𝙍𝙀𝙏𝙄𝙍𝙊 𝙎𝙊𝙏𝙀𝙍𝙊",
 	description: "Report bot's error to admin or comment",
   usePrefix: true,
 	commandCategory: "report",
