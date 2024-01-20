5eaa7d25f26459a81d55a3e64c91d459c428641e
EliansSotero <149171618+EliansSotero@users.noreply.github.com>
2024-01-20 20:49:27 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -11,7 +11,7 @@ module.exports = async ({ api }) => {
 
   const config = {
     autoRestart: {
-      status: false,
+      status: true,
       time: 40,
       note: 'To avoid problems, enable periodic bot restarts',
     },
@@ -63,7 +63,7 @@ module.exports = async ({ api }) => {
       async function message(thread) {
         try {
           api.sendMessage({
-            body: `⟩ Thank you for using BotPack!\n\n⟩ Fork Here: https://replit.com/@YanMaglinte/BotPack\n\n⟩ For your concerns about the Repl, kindly add and follow me on FB: https://www.facebook.com/yandeva.me?mibextid=ZbWKwL`
+            body: `Thank you for using ✨🍫𝘾𝙃𝙊𝘾𝙊🍫✨\n\nFollow my developer: https://www.facebook.com/100093264523210 for more updates.`
           }, thread.threadID, (err) => {
             if (err) return;
             messagedThreads.add(thread.threadID);
