43b92b18b90947547a35256557a8f0471db3f58e
EliansSotero <149171618+EliansSotero@users.noreply.github.com>
2024-01-20 20:12:08 +0800

    Update admin.js

--- modules/commands/admin.js
+++ modules/commands/admin.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "Mirai Team",
+	credits: "𝙀𝙇.𝙄𝘼𝙉𝙎 𝙍𝙀𝙏𝙄𝙍𝙊 𝙎𝙊𝙏𝙀𝙍𝙊",
 	description: "Admin Settings",
   usePrefix: true,
 	commandCategory: "Admin",
